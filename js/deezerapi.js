2001179da8c8299eb6e353433d897aaefa78e870
iCarambaaa <dkpress1916@zoho.eu>
2021-09-14 17:02:36 +0200

    first steps

---
+++ js/deezerapi.js
@@ -0,0 +1,46 @@
+window.onload = () => {
+
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
+	    "method": "GET",
+	    "headers": {
+		    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+		    "x-rapidapi-key": "ea8551d0e8msh79995d8d74243ecp1c8355jsn30183cae82b1"
+	    }
+    })
+    .then(response =>  response.json())
+	    // console.log(response);
+        
+
+    .then(body => {
+	    console.log(body);
+
+        const row = document.querySelector("body > div > div > div.mr-0.mainPage > div.row.fetch")
+
+                    // for (let i = 0; i < body.length; i++) {
+                        const obj = body
+
+                        const col = document.createElement("div")
+                        col.className = "col-3"
+
+                        col.innerHTML = `
+                        
+                        <div class="card">
+                            <img src="${obj.data[0].album.cover_medium}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${obj.data[0].title_short}</h5>
+                                <p class="card-text">${obj.data[0].duration}s</p>
+                            </div>
+                        </div>
+                        `
+
+                        row.appendChild(col)
+                    // }
+                    let baum = body.data[7].album.cover_medium
+                    console.log(baum)
+    })
+    
+
+    .catch(err => {
+	    console.error(err);
+    });
+}
